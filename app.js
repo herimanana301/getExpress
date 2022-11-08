97d3081e2707078c5fdec0f0972e943776a1aa25
Herimanana <herimanana301@gmail.com>
2022-11-08 10:06:46 +0100

    express 3 done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
-
+app.use(express.json())
 const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
@@ -18,7 +18,8 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUsersById);
-
+app.post("/api/movies", movieHandlers.postMovie)
+app.post("/api/users",usersHandlers.postUser)
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
