9a66eeaad7a074b354e425dc3b2fa49e9e52cade
Herimanana <herimanana301@gmail.com>
2022-11-10 11:08:35 +0100

    Put method

--- usersHandlers.js
+++ usersHandlers.js
@@ -40,8 +40,27 @@ const postUser = (req,res)=>{
         res.status(500).send("Error saving the user");
     })
 }
+const updateUser = (req, res)=>{
+    const id = parseInt(req.params.id)
+    const {firstname,lastname,email,city,language}=req.body
+    database
+    .query("update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",[firstname,lastname,email,city,language])
+    .then((result)=>{
+        if(result.affectedRows === 0){
+            res.status(404).send("Not Found")
+        }else{
+            res.sendStatus(204)
+        }
+    })
+    .catch((err)=>{
+        console.error(err)
+        res.sendStatus(500).send("Error editing the movie")
+    })
+
+}
 module.exports = {
     getUsers,
     getUsersById,
     postUser,
+    updateUser,
 }
