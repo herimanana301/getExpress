f9f7b848f377ee2269a9981800ada08ff40a9dd8
Herimanana <herimanana301@gmail.com>
2022-11-14 10:54:20 +0100

    Bonus quest done

--- usersHandlers.js
+++ usersHandlers.js
@@ -83,7 +83,11 @@ const userFilter = (req,res)=>{
         sqlValues.push(req.query.language)
     }
     if (req.query.city !=null){
-        sql += " where city = ?"
+        sql += " and city = ?"
+        sqlValues.push(req.query.city)
+    }
+    else if (req.query.city != null){
+        sql += "where city = ?"
         sqlValues.push(req.query.city)
     }
     database
